c23729b796258bf6675ca94d56742c5f623d1ae2
sakshiad <sakshi@cloudtechservice.com>
2024-06-28 14:40:08 +0545

    Add .gitignore

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-tfstate-cloud-tech"
+    bucket         = "terraform-tfstate-cloud-tech-qa"
     key            = "state/terraform.tfstate"
     region         = "us-east-2"
     encrypt        = true
